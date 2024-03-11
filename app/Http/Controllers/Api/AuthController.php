866c0bd893806e594580c67b2a3ade85e28ec207
Bibek <bibekstha084@gmail.com>
2024-03-11 23:40:09 +1030

    updates in register

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
+use App\Models\Team;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
@@ -16,20 +18,38 @@ class AuthController extends Controller
     {
         $data = $request->validated();
 
-        $user = User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
-            'gender' => $data['gender'],
-            'dob' =>  $data['dob'],
-            'address' =>  $data['address'],
-            'contact_no' =>  $data['contact_no'],
-            'is_leader' => true
-        ]);
+        $name = substr($data['name'], 0, 3);
+        $time = date("YmdHis");
 
-        return response([
-            'user' => $user,
-        ]);
+        try {
+            DB::beginTransaction();
+
+            $team = Team::create([
+                'name' => $name.$time
+            ]);
+
+            $user = User::create([
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'password' => bcrypt($data['password']),
+                'gender' => $data['gender'],
+                'dob' =>  $data['dob'],
+                'address' =>  $data['address'],
+                'contact_no' =>  $data['contact_no'],
+                'is_leader' => true,
+                'team_id' => $team->id
+            ]);
+
+            DB::commit();
+
+            return response([
+                'user' => $user,
+                'team' => $team
+            ]);
+        } catch (\Throwable $th) {
+            //throw $th;
+            DB::rollBack();
+        }
     }
 
     public function login(LoginRequest $request)
